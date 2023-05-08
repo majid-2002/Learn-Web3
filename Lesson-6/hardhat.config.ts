b8353c978f905b895b0dd23c8c3223c52c53d94c
Abdul Majid <majid616365@gmail.com>
2023-05-08 21:02:16 +0530

    Lesson 6: Used Hardat Learnt about different hardhat tools for advanced testing, encryption, etherscan verification using hardhat, local node creation, gasReporter generation using hardhat-gas-reporter package. Used solidity for testcase coverage auditing. Env variable and private key encryption.

---
+++ Lesson-6/hardhat.config.ts
@@ -0,0 +1,39 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+import "./tasks/block-num";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+const RPC_URL = process.env.RPC_URL || "https://eth-sepolia-rpc.herokuapp.com/";
+const PRIVATE_KEY = process.env.PRIVATE_KEY! || "0xkey";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY! || "key";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY! || "key";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    },
+
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: false,
+    outputFile: "gas-report.pdf",
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "MATIC",
+  },
+};
+
+export default config;
---
+++ Lesson-6/test/test-deploy.ts
@@ -0,0 +1,32 @@
+import { ethers } from "hardhat";
+import { expect } from "chai";
+
+describe("SimpleStorage", async () => {
+  let SimpleStorageFactory, simpleStorage: any;
+
+  beforeEach(async function () {
+    const SimpleStorageFactory = await ethers.getContractFactory(
+      "SimpleStorage"
+    );
+    simpleStorage = await SimpleStorageFactory.deploy();
+  });
+
+  it("Should start with a favorite number of 0", async () => {
+    expect(await simpleStorage.retrieve()).to.equal(0);
+  });
+
+  it("Should update the favorite number", async () => {
+    await simpleStorage.store(10);
+    expect(await simpleStorage.retrieve()).to.equal(10);
+  });
+
+  it("Should add a new person with their favorite number", async () => {
+    const name = "Alice";
+    const favoriteNumber = 42;
+
+    await simpleStorage.addPerson(name, favoriteNumber);
+
+    const storedFavoriteNumber = await simpleStorage.nameToFavoriteNumber(name);
+    expect(storedFavoriteNumber).to.equal(favoriteNumber);
+  });
+});
